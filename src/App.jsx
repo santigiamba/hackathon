7574009b63a2f2bf60b2e85eccb0db86c3b43271
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-09-03 10:18:40 -0300

    espero que funcione

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,44 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [num, setNum] = useState(0);
+  const [contactos, setContactos] = useState([]);
 
-};
+  const actualizarNombre = (evento) => {
+    setNombre(evento.target.value);
+  };
   
- return
+  const actualizarNum = (evento) => {
+    setNum(evento.target.value);
+  };
 
+  const agendar = () => {
+    setContactos([...contactos, { nombre: nombre, num: num }]);
+    console.log("listo");
+    console.log(contactos);
+  };
+  
+  return (
+    <>
+      <div className='conteinerInput'>
+        <input onChange={actualizarNombre} type="text" placeholder="Nombre" />
+        <input onChange={actualizarNum} type="number" placeholder="Número" />
+        <button onClick={agendar}>Agregar Contacto</button>
+      </div>
+      <div>
+        <h3>Lista de Contactos:</h3>
+        <ul>
+          {contactos.map((contacto, index) => (
+            <li key={index}>
+              nombre: {contacto.nombre}, numero: {contacto.num}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
 
 export default App;
 
