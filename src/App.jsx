318270302b6e2610c09413b62dd573d4dd1d8b3d
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-09-03 14:27:38 -0300

    funciona condiciones del clima

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   const [city, setCity] = useState('');
   const [error, setError] = useState(null);
   const [temp, setTemp] = useState(null);
-  const [viento, setViento] = useState(null);
+  const [windSpeed, setWindSpeed] = useState(null);
+  const [humidity, setHumidity] = useState(null)
   const [lat, setLat] = useState(null);
   const [lon, setLon] = useState(null);
-  
+
   const geocodingApiKey = "4f4cab38624d4e3aa7106718aeea913f"; 
   const weatherApiKey = "e8560341fa9e461ec8fd8b0a24819e86"; 
 
@@ -28,34 +29,37 @@ function App() {
         throw new Error('No se pudo obtener la ubicación');
       }
       const data = await response.json();
-      
+
       if (data.results.length > 0) {
         const location = data.results[0].geometry;
         setLat(location.lat);
         setLon(location.lng);
-        getTemperature(location.lat, location.lng); 
+        conditionals(location.lat, location.lng); 
       } else {
-        throw new Error('No se encontraron resultados para la ciudad ingresada');
+        throw new Error('No se encontro la ciudad');
       }
     } catch (err) {
       setError(err.message);
     }
   };
 
-  const getTemperature = async (latitude, longitude) => {
+  const conditionals = async (latitude, longitude) => {
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${weatherApiKey}`
       );
       if (!response.ok) {
-        throw new Error('No se pudo obtener la temperatura');
+        throw new Error('No se pudo obtener la información del clima');
       }
       const data = await response.json();
       setTemp(data.main.temp);
+      setWindSpeed(data.wind.speed); 
+      setHumidity(data.main.humidity);
     } catch (err) {
       setError(err.message);
     }
   };
+
 
   return (
     <div>
@@ -65,13 +69,19 @@ function App() {
         onChange={cityChange}
         placeholder="Ciudad"
       />
-      <button onClick={getCoordinates}>Obtener Temperatura</button>
+      <button onClick={getCoordinates}>Obtener Clima</button>
 
       {lat !== null && lon !== null && (
         <p>Latitud: {lat}, Longitud: {lon}</p>
       )}
       {temp !== null && (
         <p>Temperatura: {temp}°C</p>
+      )}
+      {windSpeed !== null && (
+        <p>Velocidad del viento: {windSpeed} m/s</p>
+      )}
+      {humidity !== null && (
+        <p>humedad en el ambiente: {humidity}% </p>
       )}
       {error && <p>Error: {error}</p>}
     </div>
